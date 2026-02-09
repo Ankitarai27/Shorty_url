3e0f3c04eaf8454641e390adfd44dd94e7501da7
Ankita Rai <129681815+Ankitarai27@users.noreply.github.com>
2026-02-09 09:37:23 +0000

    #updated app

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {nanoid} from "nanoid"
 import dotenv from "dotenv"
-import connectDB from "./src/config/monogo.config.js"
+import connectDB from "./src/config/monogo.config.js" // Check spelling!
 import short_url from "./src/routes/short_url.route.js"
 import user_routes from "./src/routes/user.routes.js"
 import auth_routes from "./src/routes/auth.routes.js"
@@ -11,14 +11,14 @@ import cors from "cors"
 import { attachUser } from "./src/utils/attachUser.js";
 import cookieParser from "cookie-parser"
 
-dotenv.config("./.env")
+dotenv.config() // Standard config call
 
 const app = express();
-
 app.use(cors({
-    origin: 'http://localhost:5173', // your React app
-    credentials: true // 👈 this allows cookies to be sent
+    origin: (origin, callback) => callback(null, true), // Allows any origin to connect
+    credentials: true
 }));
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -33,9 +33,9 @@ app.get("/:id",redirectFromShortUrl)
 
 app.use(errorHandler)
 
-app.listen(3000,()=>{
+// Use process.env.PORT for deployment
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     connectDB()
-    console.log("Server is running on http://localhost:3000");
+    console.log(`Server is running on port ${PORT}`);
 })
-
-// GET - Redirection 
