9078108357a6d2e33d5d9f4202102adcac977879
Ankita Rai <129681815+Ankitarai27@users.noreply.github.com>
2026-02-18 16:23:33 +0000

    updated packages

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 import {nanoid} from "nanoid"
 import dotenv from "dotenv"
 import connectDB from "./src/config/monogo.config.js" // Check spelling!
@@ -7,13 +8,21 @@ import user_routes from "./src/routes/user.routes.js"
 import auth_routes from "./src/routes/auth.routes.js"
 import { redirectFromShortUrl } from "./src/controller/short_url.controller.js";
 import { errorHandler } from "./src/utils/errorHandler.js";
-import cors from "cors"
+import cors from "cors";
 import { attachUser } from "./src/utils/attachUser.js";
-import cookieParser from "cookie-parser"
-
+import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
 dotenv.config() // Standard config call
 
+
+
+
+
 const app = express();
+app.use(rateLimit({
+ windowMs: 15*60*1000,
+ max: 100
+}));
 app.use(cors({
     origin: true,
     credentials: true
@@ -40,3 +49,5 @@ app.listen(PORT, () => {
     connectDB()
     console.log(`Server is running on port ${PORT}`);
 })
+
+app.use(helmet())
